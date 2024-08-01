8783b42b015d95161d07ef055c20e0a27ececda1
Ducho <vidrioamadorarmando11@gmail.com>
2024-08-01 14:50:38 -0600

    obtain all the jobs

--- Project/Top_skills.py
+++ Project/Top_skills.py
@@ -2,13 +2,29 @@ import requests
 import pandas as pd
 import json
 import streamlit as st
+import numpy as np
 
 st.set_page_config(
     page_title = "Data skills"
 )
 
-st.write("hola mundo")
-
 st.title("Data skills")
 
-st.sidebar.success("Elige una plataforma para ver sus peliculas y series disponibles")
+st.write("Job title:")
+
+## Global variables
+skills_options = ['All', 'Languages', 'Tools', 'Databases', 'Cloud', 'Libraries', 'Frameworks']
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write('Column 1')
+    st.selectbox('Job Title', [1,2,3])
+
+with col2:
+    st.write('Column 2')
+    st.selectbox('Country', [1,2,3])
+    st.write('X jobs analyzed')
+
+selected_skill = st.radio('Skills:', skills_options)
+
+st.write(f'The selected skill is: {selected_skill}')
--- Project/pruebas.py
+++ Project/pruebas.py
@@ -1,12 +1,56 @@
+import os
+import pandas as pd
+import json
 from serpapi import GoogleSearch
+from dotenv import load_dotenv
 
+# Cargar variables de entorno desde archivo .env
+load_dotenv()
+
+# Cargar la clave API desde las variables de entorno
+api_key = os.getenv('SERPAPI_API_KEY')
+
+# Configurar los parámetros de búsqueda
 params = {
     "engine": "google_jobs",
-    "q": "barista new york",
-    "hl": "en",
-    "api_key": "92c25ee62c44d72638b2d2667b2d7f09365033349cff919e774eed8305a4f1e8"
+    "q": "Data Engineer",
+    "location": "United States",
+    "api_key": api_key
 }
 
-search = GoogleSearch(params)
-results = search.get_dict()
-jobs_results = results["jobs_results"]
+# Crear una instancia de GoogleSearch y realizar la búsqueda
+# Inicializar variables
+total_jobs = 0
+page = 1
+
+while True:
+    # Crear una instancia de GoogleSearch y realizar la búsqueda
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    normalize_results = json.normalize(results)
+
+    # Verificar la respuesta completa
+    df = pd.DataFrame(normalize_results)
+    print(df.headers)
+
+    # Imprimir los resultados y contar los trabajos
+    jobs = results.get('jobs_results', [])
+    for job in jobs:
+        print(job)
+        total_jobs += 1
+
+    print(f"Total jobs so far: {total_jobs}")
+
+    # Verificar si hay un next_page_token
+    next_page_token = results.get('next_page_token')
+    if not next_page_token:
+        break
+
+    # Actualizar los parámetros de búsqueda con el next_page_token
+    params['next_page_token'] = next_page_token
+    page += 1
+
+print(f"Total jobs: {total_jobs}")
+
+
+
